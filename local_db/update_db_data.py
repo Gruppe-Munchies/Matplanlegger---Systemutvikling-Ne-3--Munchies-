52b69eef6c95ad4aadc99f17cd7d370b4cafa5dd
carlos reiakvam <cre032@uit.no>
2022-02-24 15:03:35 +0100

    Cleanup

--- local_db/update_db_data.py
+++ local_db/update_db_data.py
@@ -1,18 +1,28 @@
 from local_db_create import engine
 from sqlalchemy.ext.automap import automap_base
-from sqlalchemy.orm import Session
-from sqlalchemy import create_engine, select
+# from sqlalchemy.orm import Session
+# from sqlalchemy import create_engine, select
 
+# Initialise Base with engine
 Base = automap_base()
 Base.prepare(engine, reflect=True)
 
-ingredient = Base.classes.ingredient
+# Add ORM for table user
 user = Base.classes.user
-stm = select([ingredient])
 
-session = Session(engine)
-connection = engine.connect()
-results = connection.execute(stm).fetchall()
+# Query data from table user
+malvin = user.query.filter_by(username="Malvin Khan").first()
+print(malvin.email)
+print(malvin.firstname)
 
-for result in results:
-    print(result)
+# Automap approach hentet fra
+# https://docs.sqlalchemy.org/en/14/orm/extensions/automap.html
+
+# ingredient = Base.classes.ingredient
+# stm = select([ingredient])
+# session = Session(engine)
+# connection = engine.connect()
+# results = connection.execute(stm).fetchall()
+
+# for result in results:
+#     print(result)
