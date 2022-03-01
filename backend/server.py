232cf96141a17eee8e0b6f5bcdccd0fea0b355c1
Jan Erik Bisseth <jbi017@uit.no>
2022-03-01 22:19:01 +0100

    Template med resultat fra spørring

--- backend/server.py
+++ backend/server.py
@@ -1,16 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template
 from local_db.db_user_info import username, password, DB_NAME
+from local_db.test_queries import return_email_from_name
 
 # Flask configuration
-app = Flask(__name__)
+app = Flask(__name__, template_folder='C:/UiT/Python/Munchies/Matplanlegger---Systemutvikling-Ne-3--Munchies-/templates')
 app.config['SQLALCHEMY_DATABASE_URI'] = f"mysql+pymysql://{username}:{password}@localhost/{DB_NAME}"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # stops warning
 
 
-@app.route('/')
+@app.route('/test')
 def hello_world():  # put application's code here
-    return 'Hello Munchies!'
-
+    email = return_email_from_name("Jan Erik")
+    return render_template('test2.html', result=email)
 
 if __name__ == '__main__':
     app.run()
--- local_db/test_queries.py
+++ local_db/test_queries.py
@@ -1,3 +1,6 @@
+from flask import render_template
+from sqlalchemy import inspect
+
 from local_db.orm import User, Ingredient, Recipe, RecipeHasIngredient, RecipeHasWeeklyMenu, RecipeAvailability, \
     Usertype, Usergroup, UsergroupHasIngredient, WeeklyMenu, Base
 from sqlalchemy.orm import Session, declarative_base, sessionmaker
@@ -10,8 +13,12 @@ def loadSession():
     session = Session()
     return session
 
+def object_as_dict(obj):
+    return {c.key: getattr(obj, c.key)
+            for c in inspect(obj).mapper.column_attrs}
 
-# Test quieries
-session = loadSession()
-res = session.query(User).all()
-print(res[0].email)
+# Test queries
+def return_email_from_name(name):
+    session = loadSession()
+    res = session.query(User).filter(User.firstname == name).all()
+    return res
