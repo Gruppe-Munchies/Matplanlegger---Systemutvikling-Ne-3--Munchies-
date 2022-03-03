1640082e718a6c5e1db88550947a337a2de20ef8
Jan Erik Bisseth <jbi017@uit.no>
2022-03-03 16:58:45 +0100

    Metoder for å skrive til alle tabeller

--- backend/server.py
+++ backend/server.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template
 from local_db.db_user_info import username, password, DB_NAME
-from local_db.test_queries import return_email_from_name
+from local_db.test_queries import return_email_from_name, insert_to_user
 
 # Flask configuration
 app = Flask(__name__, template_folder='C:/UiT/Python/Munchies/Matplanlegger---Systemutvikling-Ne-3--Munchies-/templates')
@@ -12,6 +12,10 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # stops warning
 def hello_world():  # put application's code here
     email = return_email_from_name("Jan Erik")
     return render_template('test2.html', result=email)
+
+@app.route('/insert')
+def insert():
+    insert_to_user("brukernavn", "epost", "fornavn", "etternavn", "passord", "1", "1")
 
 if __name__ == '__main__':
     app.run()
--- local_db/test_queries.py
+++ local_db/test_queries.py
@@ -20,5 +20,79 @@ def object_as_dict(obj):
 # Test queries
 def return_email_from_name(name):
     session = loadSession()
-    res = session.query(User).filter(User.firstname == name).all()
+    res = session.query(User).where(User.firstname == name).all()
     return res
+
+#Add default usergroups
+def insert_to_usergroup():
+    session = loadSession()
+    usergroup1 = Usergroup(groupName="Restaurant")
+    usergroup2 = Usergroup(groupName="Familie")
+    session.add_all([usergroup1, usergroup2])
+    session.commit()
+
+#Add default usertypes
+def insert_to_usertype():
+    session = loadSession()
+    usertype1 = Usertype(userTypeName="Admin")
+    usertype2 = Usertype(userTypeName="Bruker")
+    session.add_all([usertype1, usertype2])
+    session.commit()
+
+#Add default values to recipe availability
+def insert_to_recipeavalilability():
+    session = loadSession()
+    avail1 = RecipeAvailability(availableFor="All")
+    avail2 = RecipeAvailability(availableFor="Group")
+    avail3 = RecipeAvailability(availableFor="User")
+    session.add_all([avail1, avail2, avail3])
+    session.commit()
+
+#Add ingredient
+def insert_to_ingredients(name):
+    session = loadSession()
+    ingredient = Ingredient(ingredientName=name)
+    session.add(ingredient)
+    session.commit()
+
+#Add user
+def insert_to_user(name, email, firstname, lastname, password, usertype, usergroup):
+    session = loadSession()
+    new_user = User(username=name, email=email, firstname=firstname, lastname=lastname, password=password, userType_iduserType=usertype, userGroup_iduserGroup=usergroup)
+    session.add(new_user)
+    session.commit()
+
+#Add recipe
+def insert_to_recipe(name, shortDescription, description, image, userGroup, recipeAvailability, weeklymenu):
+    session = loadSession()
+    new_recipe = Recipe(name=name, shortDescription=shortDescription, description=description, image=image, userGroup_iduserGroup=userGroup, recipeAvailability_idrecipeAvailability=recipeAvailability, weeklyMenu_idweeklyMenu=weeklymenu)
+    session.add(new_recipe)
+    session.commit()
+
+#Add to weeklyMenu --- NEEDS CONNECTION TO USERGROUP
+def insert_to_weeklymenu(weekNum, day, name, description):
+    session = loadSession()
+    new_weeklyMenu = WeeklyMenu(weekNum=weekNum, day=day, name=name, description=description)
+    session.add(new_weeklyMenu)
+    session.commit()
+
+#Add to recipe_has_ingredient
+def insert_to_recipe_has_ingredient(recipe, ingredient, quantity):
+    session = loadSession()
+    new_recipeIngredient = RecipeHasIngredient(recipe_idRecipe=recipe, ingredient_idingredient=ingredient, quantity=quantity)
+    session.add(new_recipeIngredient)
+    session.commit()
+
+#Add to recipe_has_weeklyMenu
+def insert_to_recipe_has_weeklymenu(recipe, year, week, expectedConsumption, actualConsumption):
+    session = loadSession()
+    new_recipeWeeklymenu = RecipeHasWeeklyMenu(recipe_idRecipe=recipe, weeklyMenu_year=year, weeklyMenu_weekNum=week, expectedConsumption=expectedConsumption, actualConsumption=actualConsumption)
+    session.add(new_recipeWeeklymenu)
+    session.commit()
+
+#Add to usergroup_has_ingredient
+def insert_to_usergroup_has_ingredient(userGroup, ingredient, price, unit):
+    session = loadSession()
+    new_userGroupIngredient = UsergroupHasIngredient(userGroup_iduserGroup=userGroup, ingredient_idingredient=ingredient, price=price, unit=unit)
+    session.add(new_userGroupIngredient)
+    session.commit()
