bfd87c44837fed0ac69b364631d02ac103d23cfa
carlos reiakvam <cre032@uit.no>
2022-03-06 17:01:39 +0100

    Move server.py to root, make template folder dir relative

--- server.py
+++ server.py
@@ -7,9 +7,8 @@ from local_db.db_user_info import username, password, DB_NAME
 from local_db.insert_to_db import return_email_from_name, insert_to_user
 from local_db.db_testData import test_data
 
-
 # Flask configuration
-app = Flask(__name__, template_folder='C:/UiT/Python/Munchies/Matplanlegger---Systemutvikling-Ne-3--Munchies-/templates')
+app = Flask(__name__)
 app.register_blueprint(backend.main.views.mainpage)
 app.register_blueprint(backend.auth.views.auth)
 app.config['SQLALCHEMY_DATABASE_URI'] = f"mysql+pymysql://{username}:{password}@localhost/{DB_NAME}"
@@ -17,13 +16,16 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # stops warning
 app.config['SECRET_KEY'] = "secretkey"
 app.config['WTF_CSRF_SECRET_KEY'] = "secretkey"
 
+
 @app.route('/')
 def index():
-    return "Test"
+    return "noe annet"
+
 
 @app.route('/testdata')
 def testdata():
     test_data()
+
 
 if __name__ == '__main__':
     app.run()
