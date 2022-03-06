b56d4ce041413f54844e7db322674f4931657c98
Jan Erik Bisseth <jbi017@uit.no>
2022-03-06 17:47:24 +0100

    WTForm for ingredient

---
+++ backend/ingredients/forms.py
@@ -0,0 +1,26 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Length, Email, EqualTo
+
+class RegisterForm(FlaskForm):
+    ingredientName = StringField(
+        label=('Navn på ingrediens'),
+        validators=[DataRequired()])
+
+    usergroup = StringField(
+        label=('Brukergruppe'),
+        validators=[DataRequired()])
+
+    ingredientID = StringField(
+        label=('IngrediensID (make hidden)'),
+        validators=[DataRequired()])
+
+    price = StringField(
+        label=('Pris per enhet'),
+        validators=[DataRequired()])
+
+    unit = PasswordField(
+        label=('Enhet'),
+        validators=[DataRequired()])
+
+    submit = SubmitField(label=('Registrer ingrediens'))
