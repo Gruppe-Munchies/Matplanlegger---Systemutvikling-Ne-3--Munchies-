599f694e0b744bffdad07303d9f7cefd971ec199
carlos reiakvam <cre032@uit.no>
2022-03-13 20:13:01 +0100

    change something in file some_important_file.py

--- some_important_file.py
+++ some_important_file.py
@@ -14,23 +14,23 @@ def login():
 
 @auth.route('/register', methods=['GET', 'POST'])
 def register():
-    form = RegisterForm(request.form)
+    formA = RegisterForm(request.form)
     user_group = fetchAllUserGroups()
     all_users = fetchAllUsers()
-    if request.method == 'POST' and form.validate():
-        username = form.username.data
+    if request.method == 'POST' and formA.validate():
+        username = formA.username.data
         bruker = fetchUser(username)
         if bruker:
             flash("Brukernavn er allerede tatt", "danger")
-            return render_template('register.html', form=form, heading="Registrer ny bruker")
-        email = form.email.data
-        firstname = form.firstname.data
-        lastname = form.lastname.data
-        password = form.password.data #TODO Hash password
-        usergroup = form.usergroup.data
-        usertype = form.usertype.data
+            return render_template('register.html', form=formA, heading="Registrer ny bruker")
+        email = formA.email.data
+        firstname = formA.firstname.data
+        lastname = formA.lastname.data
+        password = formA.password.data #TODO Hash password
+        usergroup = formA.usergroup.data
+        usertype = formA.usertype.data
 
-        #TODO Usertype should be 1 (admin) as standard when usergroup is created, else 2 (normal user)
+        #TODO ENDRING_FRA_A Usertype should be 1 (admin) as standard when usergroup is created, else 2 (normal user)
 
         #Insert user to database
         auth_queries.insert_to_user(username, email, firstname, lastname, password)
@@ -51,13 +51,18 @@ def register():
         flash('Registreringen var vellykket!')
         return redirect(url_for("auth.register"))
 
-    for fieldName, error_messages in form.errors.items():
+    for fieldName, error_messages in formA.errors.items():
         for error_message in error_messages:
             flash(f"{error_message}", "danger")
 
-    return render_template('register.html', form=form, ug=user_group, users=all_users)
+    return render_template('register.html', form=formA, ug=user_group, users=all_users)
 
 def is_safe_url(target):
+    ref_url = urlparse(request.host_url)
+    test_url = urlparse(urljoin(request.host_url, target))
+    return test_url.scheme in ('http', 'https') and ref_url.netloc == test_url.netloc
+
+def metode_fra_a(target):
     ref_url = urlparse(request.host_url)
     test_url = urlparse(urljoin(request.host_url, target))
     return test_url.scheme in ('http', 'https') and ref_url.netloc == test_url.netloc
