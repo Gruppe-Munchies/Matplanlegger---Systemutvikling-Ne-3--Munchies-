20f76b95c4203edda3a039e8254ae5d4e88d75b9
Hanstein Rommerud <hro047@uit.no>
2022-03-14 21:14:29 +0100

    Fungerer nå

--- backend/ingredients/views.py
+++ backend/ingredients/views.py
@@ -15,23 +15,27 @@ def new():
     form = RegisterForm(request.form)
     if request.method == 'POST' and form.validate():
         ingredientName = form.ingredientName.data
-        ingredient = fetchIngredient(ingredientName)
-        if ingredient: #Sjekker om ingrediens finnes.
-            flash("Ingrediens er allerede registrert", "danger")
-            print("Ingrediens er allerede registrert")
-            return render_template('newingredient.html', form=form, heading="Registrer ny ingrediens") #vet ikke om heading trengs?
+        check_ingredient = fetchIngredient(ingredientName)
+
         usergroup = form.usergroup.data
 
         fetchedusergroup = fetchUserGroup(usergroup) # TODO bør være en dropdown der brukeren kan velge
         fetchedusergroup_ID = fetchedusergroup.iduserGroup
 
+        if check_ingredient: #Sjekker om ingrediens finnes.
+            flash("Ingrediens er allerede registrert", "danger")
+            print("Ingrediens er allerede registrert")
+            return render_template('newingredient.html', form=form, heading="Registrer ny ingrediens") #vet ikke om heading trengs?
 
-        fetchedingredientID = fetchIngredient(ingredientName) #form.ingredientID.data  # TODO Fetch ingredientID from new or existing
+        ingredient_queries.insert_to_ingredients(ingredientName)
+
+        fetchedingredientID = fetchIngredient(ingredientName)  # TODO Fetch ingredientID from new or existing
         ingredientID = fetchedingredientID.idingredient
+
         price = form.price.data
         unit = form.unit.data
 
-        ingredient_queries.insert_to_ingredients(ingredientName)
+
         auth_queries.insert_to_usergroup_has_ingredient(fetchedusergroup_ID, ingredientID, price, unit)
 
         flash('Ingrediensen er registrert!!')
