0fc0ac4acecba03974163976f2d5f4ba61eadd0d
carlos reiakvam <cre032@uit.no>
2022-03-15 16:05:32 +0100

    More working queries

--- backend/ingredients/queries.py
+++ backend/ingredients/queries.py
@@ -15,44 +15,40 @@ from local_db.orm import User, Ingredient, Recipe, RecipeHasIngredient, RecipeHa
 
 session = loadSession()
 
+
 # Add ingredient
 def insert_to_ingredients(name: str):
-    name = name.lower().capitalize()
+    # name = name.lower().capitalize() #Endrer navn til stor forbokstav. Kan fikses i html med js?
     ingredient = Ingredient(ingredientName=name)
     session.add(ingredient)
     session.commit()
 
 
-def fetch_ingredient_by_name(ingredient) -> Ingredient:
-    result = session.query(Ingredient).where(Ingredient.ingredientName == ingredient).first()
+def fetch_ingredient_by_exact_name(ingredient) -> Ingredient:
     # res = session.query(User).filter_by(username=user_name).values(text("userId"))
-    return result
+    return session.query(Ingredient).where(Ingredient.ingredientName == ingredient).first()
 
 
 def fetch_ingredient_ID_where_name_equals(ingredient_name):
-    session = loadSession()
-    result = session.query(Ingredient.idingredient).where(Ingredient.ingredientName == ingredient_name).first()
-    return result[0]
-
-
+    return session.query(Ingredient.idingredient).where(Ingredient.ingredientName == ingredient_name).first()
 
 
 def fetch_all_ingredients():
-    session = loadSession()
-
-    pass
-
-# method testing
-if __name__ == '__main__':
-    ape : Ingredient = fetch_ingredient_by_name('Egg')
-    print(ape.idingredient)
-
-def fetch_ingredients_where_exact_name_equals(name: str):
-    pass
+    return session.query(Ingredient).all()
 
 
 def fetch_ingredients_where_unit_equals(unit: str):
-    pass
+    idingredient_by_query = session.query(UsergroupHasIngredient.ingredient_idingredient).where(
+        UsergroupHasIngredient.unit == unit)
+    return session.query(Ingredient).where(Ingredient.idingredient == idingredient_by_query.label('id_ingredient'))
+    # usikker på hvorfor .label må være med men får warning ellers
+
+
+# method testing
+if __name__ == '__main__':
+    ape = fetch_ingredients_where_unit_equals("stk")
+    for ing in ape:
+        print(ing.ingredientName)
 
 
 def fetch_ingredients_where_n_ingredients_equals(n_ingredients: int):
