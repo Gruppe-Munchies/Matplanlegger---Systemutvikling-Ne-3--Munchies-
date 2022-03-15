699d4bea1429e173990a102d55a0c707bee4f7eb
carlos reiakvam <cre032@uit.no>
2022-03-15 13:06:52 +0100

    Add some query methods

--- backend/ingredients/queries.py
+++ backend/ingredients/queries.py
@@ -4,12 +4,33 @@ from local_db.orm import User, Ingredient, Recipe, RecipeHasIngredient, RecipeHa
     Usertype, Usergroup, UsergroupHasIngredient, WeeklyMenu, Base, UserHasUsergroup
 
 
-#Add ingredient
+# Queries needed for current issue
+
+def fetch_all_ingredients():
+    pass
+
+def fetch_ingredients_where_exact_name_equals(name):
+    pass
+
+# fetch_ingredient_where
+    # unit_equals(unit)=,
+    # n_ingredients_equal(n_ingredients),
+    # price_equals(price)
+    #ingredients_where_usergroup_equals(usergroup),
+    #ingredients_where_name_contains(name)
+        # greater than & less than for unit, n_ingredients, price
+
+# Queries needed for future issues
+# fetch_ingredient_in_current_weekly_menu # Postponed unitil weekly menu done
+# Queries for report # Will be addressed in separate issue regarding reports
+
+# Add ingredient
 def insert_to_ingredients(name):
     session = loadSession()
     ingredient = Ingredient(ingredientName=name)
     session.add(ingredient)
     session.commit()
+
 
 def fetchIngredient(ingredient):
     session = loadSession()
