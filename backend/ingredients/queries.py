61a5585868563fa5553020673cfd413eda54d4af
carlos reiakvam <cre032@uit.no>
2022-03-15 13:13:34 +0100

    Add query methods without returns

--- backend/ingredients/queries.py
+++ backend/ingredients/queries.py
@@ -3,26 +3,8 @@ from local_db.session import loadSession
 from local_db.orm import User, Ingredient, Recipe, RecipeHasIngredient, RecipeHasWeeklyMenu, RecipeAvailability, \
     Usertype, Usergroup, UsergroupHasIngredient, WeeklyMenu, Base, UserHasUsergroup
 
-
-# Queries needed for current issue
-
-def fetch_all_ingredients():
-    pass
-
-def fetch_ingredients_where_exact_name_equals(name):
-    pass
-
-# fetch_ingredient_where
-    # unit_equals(unit)=,
-    # n_ingredients_equal(n_ingredients),
-    # price_equals(price)
-    #ingredients_where_usergroup_equals(usergroup),
-    #ingredients_where_name_contains(name)
-        # greater than & less than for unit, n_ingredients, price
-
-# Queries needed for future issues
-# fetch_ingredient_in_current_weekly_menu # Postponed unitil weekly menu done
-# Queries for report # Will be addressed in separate issue regarding reports
+# TODO: fetch_ingredient_in_current_weekly_menu # Postponed unitil weekly menu done
+# TODO: Queries for report # Will be addressed in separate issue regarding reports
 
 # Add ingredient
 def insert_to_ingredients(name):
@@ -37,3 +19,49 @@ def fetchIngredient(ingredient):
     res = session.query(Ingredient).where(Ingredient.ingredientName == ingredient).first()
     # res = session.query(User).filter_by(username=user_name).values(text("userId"))
     return res
+
+def fetch_all_ingredients():
+    pass
+
+
+def fetch_ingredients_where_exact_name_equals(name: str):
+    pass
+
+
+def fetch_ingredients_where_unit_equals(unit: str):
+    pass
+
+
+def fetch_ingredients_where_n_ingredients_equals(n_ingredients: int):
+    pass
+
+
+def fetch_ingredients_where_price_equals(price: int):
+    pass
+
+
+def fetch_ingredients_where_usergroup_equals(usergroup):
+    pass
+
+
+def fetch_ingredients_where_name_contains(name):
+    pass
+
+
+def fetch_ingredients_where_n_ingredients_is_greater_than(n_ingredients):
+    pass
+
+
+def fetch_ingredients_where_n_ingredients_is_less_than(n_ingredients):
+    pass
+
+
+def fetch_ingredients_where_price_is_greater_than(n_ingredients):
+    pass
+
+
+def fetch_ingredients_where_price_is_less_than(n_ingredients):
+    pass
+
+
+
