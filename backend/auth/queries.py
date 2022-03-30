d88a39e507a4907c730dc1e6606458f9659f7707
Silje Aas <saa049@uit.no>
2022-03-30 15:38:35 +0200

    Query, remove user from usergroup, table UserHasUsergroup

--- backend/auth/queries.py
+++ backend/auth/queries.py
@@ -2,6 +2,8 @@ from local_db.session import loadSession
 from local_db.orm import User, Ingredient, Recipe, RecipeHasIngredient, RecipeHasWeeklyMenu, RecipeAvailability, \
     Usertype, Usergroup, UsergroupHasIngredient, WeeklyMenu, Base, UserHasUsergroup
 from werkzeug.security import generate_password_hash, check_password_hash
+from sqlalchemy import delete, and_
+
 
 #############
 #    User   #
@@ -13,9 +15,11 @@ def fetchUser(user_name):
     res = session.query(User).where(User.username == user_name).first()
     return res
 
-def fetchUserTypeByUserIdAndGroupId(user_id,user_group):
+
+def fetchUserTypeByUserIdAndGroupId(user_id, user_group):
     session = loadSession()
-    res = session.query(UserHasUsergroup).where((UserHasUsergroup.user_userId == user_id) & (UserHasUsergroup.userGroup_iduserGroup == user_group)).first()
+    res = session.query(UserHasUsergroup).where(
+        (UserHasUsergroup.user_userId == user_id) & (UserHasUsergroup.userGroup_iduserGroup == user_group)).first()
     return res.userType_iduserType
 
 
@@ -23,6 +27,7 @@ def fetchUserById(user_id):
     session = loadSession()
     res = session.query(User).where(User.id == user_id).first()
     return res
+
 
 # Fetch email from username
 def return_email_from_name(name):
@@ -52,15 +57,13 @@ def insert_to_user(name, email, firstname, lastname, password):
 # fetch_usergroups_associated_with_user(user_id) - to be used in ingredients
 # fetch_role_in_usergroup(user_id, usergroup) - to see which role the user has in a certain group.
 
-#fetch_firstname_user(user_id) - or should we use username? user_id is primary key. This is for showing who's logged in.
-#fetch_lasttname_user(user_id)
+# fetch_firstname_user(user_id) - or should we use username? user_id is primary key. This is for showing who's logged in.
+# fetch_lasttname_user(user_id)
 # compare hashed password  (login)
 
 # optional: change email, firstname, lastname, password. - one function, or many? Have to be reviewed
-#this could be a form for the user to administrate own settings.
-#when updated, use session.commit( values..?)
-
-
+# this could be a form for the user to administrate own settings.
+# when updated, use session.commit( values..?)
 
 
 #############
@@ -68,27 +71,31 @@ def insert_to_user(name, email, firstname, lastname, password):
 #############
 
 def userGroup():
-    session = loadSession() # kobler til databasen
-    res = session.query(Usergroup).all() #henter ut fra tabell Usergroup (via orm.py i local_db)
-    return res #henter ut alle kolonnene i denne tabellen
+    session = loadSession()  # kobler til databasen
+    res = session.query(Usergroup).all()  # henter ut fra tabell Usergroup (via orm.py i local_db)
+    return res  # henter ut alle kolonnene i denne tabellen
+
 
 def fetchAllUsers():
     session = loadSession()  # kobler til databasen
     res = session.query(User).all()  # henter ut fra tabell User (via orm.py i local_db)
     return res  # henter ut alle kolonnene i denne tabellen
 
+
 def fetchAllUserGroups():
     session = loadSession()  # kobler til databasen
     res = session.query(Usergroup).all()  # henter ut fra tabell Usergroup (via orm.py i local_db)
     return res  # henter ut alle kolonnene i denne tabellen
 
+
 def fetchAllUserGroupsUserHas(user_id):
     session = loadSession()
     res = session.query(Usergroup, User, Usertype).join(UserHasUsergroup,
-                                                        Usergroup.iduserGroup == UserHasUsergroup.userGroup_iduserGroup).join(User,
-                                                        User.id == UserHasUsergroup.user_userId).join(Usertype,
-                                                        Usertype.iduserType == UserHasUsergroup.userType_iduserType).filter(
-                                                        User.id == user_id).all()
+                                                        Usergroup.iduserGroup == UserHasUsergroup.userGroup_iduserGroup).join(
+        User,
+        User.id == UserHasUsergroup.user_userId).join(Usertype,
+                                                      Usertype.iduserType == UserHasUsergroup.userType_iduserType).filter(
+        User.id == user_id).all()
     return res
 
 
@@ -98,28 +105,33 @@ def fetchUserGroup(group_name):
     # res = session.query(Usergroup).filter_by(groupName=group_name).values(text("iduserGroup"))
     return res
 
+
 def fetchGroupsWhereUserHaveAdmin(username):
-    #TODO: Query for fetching groups where user have Admin-type
+    # TODO: Query for fetching groups where user have Admin-type
     pass
 
+
 def fetchUsersInUsergroup(group_name):
-    #TODO: Query for fetching all users belonging to a group
+    # TODO: Query for fetching all users belonging to a group
     session = loadSession()
     res = session.query(Usergroup, User, Usertype).join(UserHasUsergroup,
-                                                        Usergroup.iduserGroup == UserHasUsergroup.userGroup_iduserGroup).join(User,
-                                                        User.id == UserHasUsergroup.user_userId).join(Usertype,
-                                                        Usertype.iduserType == UserHasUsergroup.userType_iduserType).filter(
-                                                        Usergroup.groupName == group_name).all()
+                                                        Usergroup.iduserGroup == UserHasUsergroup.userGroup_iduserGroup).join(
+        User,
+        User.id == UserHasUsergroup.user_userId).join(Usertype,
+                                                      Usertype.iduserType == UserHasUsergroup.userType_iduserType).filter(
+        Usergroup.groupName == group_name).all()
     return res
 
+
 def fetchUsersInUsergroupById(group_id):
-    #TODO: Query for fetching all users belonging to a group
+    # TODO: Query for fetching all users belonging to a group
     session = loadSession()
     res = session.query(Usergroup, User, Usertype).join(UserHasUsergroup,
-                                                        Usergroup.iduserGroup == UserHasUsergroup.userGroup_iduserGroup).join(User,
-                                                        User.id == UserHasUsergroup.user_userId).join(Usertype,
-                                                        Usertype.iduserType == UserHasUsergroup.userType_iduserType).filter(
-                                                        Usergroup.iduserGroup == group_id).all()
+                                                        Usergroup.iduserGroup == UserHasUsergroup.userGroup_iduserGroup).join(
+        User,
+        User.id == UserHasUsergroup.user_userId).join(Usertype,
+                                                      Usertype.iduserType == UserHasUsergroup.userType_iduserType).filter(
+        Usergroup.iduserGroup == group_id).all()
     return res
 
 
@@ -128,10 +140,12 @@ def fetchUserType(usertype):
     res = session.query(Usertype).where(Usertype.userTypeName == usertype).first()
     return res
 
+
 def fetchAllUserTypes():
     session = loadSession()
     res = session.query(Usertype).all()
     return res
+
 
 # Add default usergroups
 def insert_to_usergroup(name):
@@ -161,4 +175,11 @@ def insert_to_usertype():
     usertype1 = Usertype(userTypeName="Admin")
     usertype2 = Usertype(userTypeName="Bruker")
     session.add_all([usertype1, usertype2])
+    session.commit()
+
+
+# Remove user from usergroup
+def remove_row_from_UserHasUsergroup(user_id, usergroup_id):
+    session = loadSession()
+    session.query(UserHasUsergroup).filter(and_(UserHasUsergroup.user_userId == user_id, UserHasUsergroup.userGroup_iduserGroup == usergroup_id)).delete(synchronize_session = False)
     session.commit()
