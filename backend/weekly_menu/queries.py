1df8025e1d4fa690127865a60b3133e77a2244db
Silje Aas <saa049@uit.no>
2022-04-06 14:34:00 +0200

    Updated query to incert weekly menu, new query to get all recipes for a usergroup

--- backend/weekly_menu/queries.py
+++ backend/weekly_menu/queries.py
@@ -3,9 +3,16 @@ from local_db.session import loadSession
 from local_db.orm import User, Ingredient, Recipe, RecipeHasIngredient, RecipeHasWeeklyMenu, RecipeAvailability, \
     Usertype, Usergroup, UsergroupHasIngredient, WeeklyMenu, Base, UserHasUsergroup
 
+session = loadSession()
+
 #Add to weeklyMenu
-def insert_to_weeklymenu(year, weekNum, day, name, description, usergroup):
+def insert_to_weeklymenu(name, description, usergroup):
     session = loadSession()
-    new_weeklyMenu = WeeklyMenu(year=year, weekNum=weekNum, day=day, name=name, description=description, userGroup_iduserGroup=usergroup)
+    new_weeklyMenu = WeeklyMenu(name=name, description=description, userGroup_iduserGroup=usergroup)
     session.add(new_weeklyMenu)
     session.commit()
+
+
+def fetch_recipes_where_usergroupid(usergroupId):
+    return session.query(Recipe).where(Recipe.userGroup_iduserGroup == usergroupId).all()
+
