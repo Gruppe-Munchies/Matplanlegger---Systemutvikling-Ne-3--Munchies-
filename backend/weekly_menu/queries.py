d8da029e4b9a0b4e7912e7b79b227ab55fd2348f
Silje Aas <saa049@uit.no>
2022-04-08 10:59:15 +0200

    Sjekker om gruppe har ukesmeny med samme navn, sender tilbake til hovedside for ukesmeny om success, blir på side hvis ikke godkjent.

--- backend/weekly_menu/queries.py
+++ backend/weekly_menu/queries.py
@@ -1,11 +1,11 @@
 from local_db.session import loadSession
-
 from local_db.orm import User, Ingredient, Recipe, RecipeHasIngredient, RecipeHasWeeklyMenu, RecipeAvailability, \
     Usertype, Usergroup, UsergroupHasIngredient, WeeklyMenu, Base, UserHasUsergroup
+from sqlalchemy import and_
 
 session = loadSession()
 
-#Add to weeklyMenu
+
 def insert_to_weeklymenu(name, description, usergroup):
     session = loadSession()
     new_weeklyMenu = WeeklyMenu(name=name, description=description, userGroup_iduserGroup=usergroup)
@@ -16,3 +16,7 @@ def insert_to_weeklymenu(name, description, usergroup):
 def fetch_recipes_where_usergroupid(usergroupId):
     return session.query(Recipe).where(Recipe.userGroup_iduserGroup == usergroupId).all()
 
+
+def fetch_weeklymenu_where_name_and_usergroupid(usergroup_id, menu_name):
+    return session.query(WeeklyMenu).filter(
+        and_(WeeklyMenu.userGroup_iduserGroup == usergroup_id, WeeklyMenu.name == menu_name)).all()
--- backend/weekly_menu/views.py
+++ backend/weekly_menu/views.py
@@ -1,15 +1,5 @@
-from urllib.parse import urljoin, urlparse
-
 import flask
 from flask import Blueprint, abort, flash, redirect, render_template, request, url_for, session
-from flask_login import login_required, login_user, logout_user
-import backend.recipes.queries as ingr_queries
-import backend.recipes.queries as recipes
-from backend import recipes
-from backend.auth.forms import LoginForm
-from backend.auth.queries import fetchUserGroupById, fetch_first_usergroups_for_user, fetchUser
-from backend.ingredients.queries import *
-import re
 import backend.weekly_menu.queries as weekly
 from backend.weekly_menu.forms import RegisterWeeklymenuForm
 
@@ -21,20 +11,27 @@ def legg_til_ukesmeny():
 
     group_id = flask.session.get('group_to_use', 'not set')
 
-    # for å kunne legge til oppskrifter til gruppa
+    # This variable contains all recipes of the group ---RECIPES
     group_recipes = weekly.fetch_recipes_where_usergroupid(group_id)
 
     form = RegisterWeeklymenuForm(request.form)
 
+    # TODO: Validering av form?
     if request.method == 'POST':
         weeklymanu_name = form.weekly_name.data
         weeklymenu_desc = form.weekly_desc.data
 
-        # TODO: Legg til oppskrifter
+        # Do group already have menu with same name?
+        if not weekly.fetch_weeklymenu_where_name_and_usergroupid(group_id, weeklymanu_name):
 
-        # legg til navn, beskrivelse og gruppe id til database
-        weekly.insert_to_weeklymenu(weeklymanu_name, weeklymenu_desc, group_id)
+            weekly.insert_to_weeklymenu(weeklymanu_name, weeklymenu_desc, group_id)
+
+            # TODO: Legg til oppskrifter ---RECIPES
+
+            flash("Meny lagt til!")
+            return redirect(url_for("ukesmeny"))
+        else:
+            flash("Dere har allerede en meny med dette navnet")
 
     return render_template('newWeeklyMenu.html', form=form)
-    # return redirect(url_for("weeklyMenu.legg_til_ukesmeny"))
 
