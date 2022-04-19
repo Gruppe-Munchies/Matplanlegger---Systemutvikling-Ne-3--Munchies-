55609c44f75586e2f6c9bfaac27f6216cd289723
Jan Erik Bisseth <jbi017@uit.no>
2022-04-19 20:09:28 +0200

    Queries

--- backend/reports/queries.py
+++ backend/reports/queries.py
@@ -14,13 +14,40 @@ from sqlalchemy import *
 def ingredients_used_per_week_total(groupId, year, weeknum):
     session = loadSession()
 
-    res = session.query(WeeklyMenuDate, WeeklyMenu, Usergroup, Recipe)\
-        .join(WeeklyMenuDate, WeeklyMenuDate.id_weekly_menu_date == WeeklyMenu.idWeeklyMenu)\
-        .where(WeeklyMenuDate.year == year, WeeklyMenuDate.weekNumber == weeknum)
+    res = session.query(WeeklyMenuDate.id_weekly_menu_date, WeeklyMenu.name.label("Ukemeny"),
+                        Usergroup.groupName.label("Gruppe"), RecipeHasWeeklyMenu.weeklyMenu_idWeeklyMenu,
+                        func.sum(RecipeHasWeeklyMenu.expectedConsumption).label("Antall retter"), Recipe.name.label("Oppskrift"),
+                        RecipeHasIngredient.recipe_idRecipe, Ingredient.ingredientName.label("Ingrediens"), UsergroupHasIngredient,
+                        func.sum(RecipeHasIngredient.quantity).label("Mengde per rett"),
+                        (func.sum(RecipeHasWeeklyMenu.expectedConsumption * RecipeHasIngredient.quantity)).label("Sum"))\
+        .join(WeeklyMenu, WeeklyMenuDate.id_weekly_menu_date == WeeklyMenu.idWeeklyMenu)\
+        .join(Usergroup, WeeklyMenu.userGroup_iduserGroup == Usergroup.iduserGroup)\
+        .join(RecipeHasWeeklyMenu, RecipeHasWeeklyMenu.weeklyMenu_idWeeklyMenu == WeeklyMenu.idWeeklyMenu)\
+        .join(Recipe, Recipe.idRecipe == RecipeHasWeeklyMenu.recipe_idRecipe)\
+        .join(RecipeHasIngredient, RecipeHasIngredient.recipe_idRecipe == Recipe.idRecipe)\
+        .join(Ingredient, Ingredient.idingredient == RecipeHasIngredient.ingredient_idingredient)\
+        .join(UsergroupHasIngredient, UsergroupHasIngredient.ingredient_idingredient == Ingredient.idingredient)\
+        .where(WeeklyMenuDate.year == year, WeeklyMenuDate.weekNumber == weeknum, WeeklyMenu.userGroup_iduserGroup == groupId)\
+        .group_by(Usergroup.iduserGroup, Ingredient.idingredient)
 
     session.close()
     return res
 
-def ingredients_user_per_week_per_dish(groupId, year, weeklyMenuId):
+
+def ingredients_used_per_week_per_dish(groupId, year, weeknum):
     session = loadSession()
 
+    res = session.query(WeeklyMenuDate.id_weekly_menu_date, WeeklyMenu.name.label("Ukemeny"), Usergroup.groupName.label("Gruppe"),
+                        RecipeHasWeeklyMenu.weeklyMenu_idWeeklyMenu, RecipeHasWeeklyMenu.expectedConsumption.label("Prognose"),
+                        Recipe.name.label("Oppskrift"), RecipeHasIngredient.recipe_idRecipe, Ingredient.ingredientName.label("Ingrediens"),
+                        RecipeHasIngredient.quantity.label("Mengde"), (RecipeHasWeeklyMenu.expectedConsumption * RecipeHasIngredient.quantity).label("Sum"))\
+        .join(WeeklyMenu, WeeklyMenuDate.id_weekly_menu_date == WeeklyMenu.idWeeklyMenu)\
+        .join(Usergroup, WeeklyMenu.userGroup_iduserGroup == Usergroup.iduserGroup)\
+        .join(RecipeHasWeeklyMenu, RecipeHasWeeklyMenu.weeklyMenu_idWeeklyMenu == WeeklyMenu.idWeeklyMenu)\
+        .join(Recipe, Recipe.idRecipe == RecipeHasWeeklyMenu.recipe_idRecipe)\
+        .join(RecipeHasIngredient, RecipeHasIngredient.recipe_idRecipe == Recipe.idRecipe)\
+        .join(Ingredient, Ingredient.idingredient == RecipeHasIngredient.ingredient_idingredient)\
+        .where(WeeklyMenuDate.year == year, WeeklyMenuDate.weekNumber == weeknum, WeeklyMenu.userGroup_iduserGroup == groupId)
+
+    session.close()
+    return res
--- backend/reports/views.py
+++ backend/reports/views.py
@@ -1,13 +1,23 @@
 from flask import Blueprint
 from backend.reports.queries import *
+import pandas as pd
+from flask_alchemy_db_creation.local_db_create import engine
 
 reports = Blueprint('reports', __name__, template_folder='templates')
 
 def test():
-    res = ingredients_used_per_week_total(1,2022,16)
+    pd.set_option('display.max_columns', None)
+    pd.set_option('display.expand_frame_repr', False)
 
-    for i in res:
-        print(i)
+    res = pd.read_sql(ingredients_used_per_week_per_dish(1,2022,16).statement, engine)
+    print(res[['Ukemeny','Gruppe', 'Oppskrift','Prognose', 'Ingrediens', 'Mengde', 'Sum']])
+
+    print("")
+    print("")
+
+    res2 = pd.read_sql(ingredients_used_per_week_total(1,2022,16).statement, engine)
+    print(res2[['Ukemeny','Gruppe', 'Antall retter', 'Ingrediens', 'Sum']])
+
 
 if __name__ == '__main__':
     test()
