18a5a9af441f20fd99fb95fbfb9fa370bbe633f2
Jan Erik Bisseth <jbi017@uit.no>
2022-04-19 16:14:49 +0200

    Queries skeleton

--- backend/reports/queries.py
+++ backend/reports/queries.py
@@ -4,3 +4,20 @@ from local_db.orm import User, Ingredient, Recipe, RecipeHasIngredient, RecipeHa
     Usertype, Usergroup, UsergroupHasIngredient, WeeklyMenu, Base, UserHasUsergroup
 from sqlalchemy import *
 
+# Queries needed:
+    # Per week
+        # Ingredients used, totalt and per dish
+        # Costs, total and per dish
+
+    # Per month (if possible)
+        # Ingredients used, total and per dish
+        # Costs, total and per dish
+
+
+# Ingredients used per week
+def ingredients_used_per_week_total(groupId, weeknum):
+    session = loadSession()
+
+def ingredients_user_per_week_per_dish(groupId, weeknum):
+    session = loadSession()
+
