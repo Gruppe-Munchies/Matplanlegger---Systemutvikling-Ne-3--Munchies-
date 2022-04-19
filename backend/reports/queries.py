d5228f29ca8051af99e3f887a4e134a0282c2ac2
Jan Erik Bisseth <jbi017@uit.no>
2022-04-19 16:44:38 +0200

    Testing query

--- backend/reports/queries.py
+++ backend/reports/queries.py
@@ -1,7 +1,7 @@
 from local_db.session import loadSession
 
 from local_db.orm import User, Ingredient, Recipe, RecipeHasIngredient, RecipeHasWeeklyMenu, RecipeAvailability, \
-    Usertype, Usergroup, UsergroupHasIngredient, WeeklyMenu, Base, UserHasUsergroup
+    Usertype, Usergroup, UsergroupHasIngredient, WeeklyMenu, Base, UserHasUsergroup, WeeklyMenuDate
 from sqlalchemy import *
 
 # Queries needed:
@@ -9,15 +9,18 @@ from sqlalchemy import *
         # Ingredients used, totalt and per dish
         # Costs, total and per dish
 
-    # Per month (if possible)
-        # Ingredients used, total and per dish
-        # Costs, total and per dish
-
 
 # Ingredients used per week
-def ingredients_used_per_week_total(groupId, weeknum):
+def ingredients_used_per_week_total(groupId, year, weeknum):
     session = loadSession()
 
-def ingredients_user_per_week_per_dish(groupId, weeknum):
+    res = session.query(WeeklyMenuDate, WeeklyMenu, Usergroup, Recipe)\
+        .join(WeeklyMenuDate, WeeklyMenuDate.id_weekly_menu_date == WeeklyMenu.idWeeklyMenu)\
+        .where(WeeklyMenuDate.year == year, WeeklyMenuDate.weekNumber == weeknum)
+
+    session.close()
+    return res
+
+def ingredients_user_per_week_per_dish(groupId, year, weeklyMenuId):
     session = loadSession()
 
--- backend/reports/views.py
+++ backend/reports/views.py
@@ -1,3 +1,13 @@
 from flask import Blueprint
+from backend.reports.queries import *
 
 reports = Blueprint('reports', __name__, template_folder='templates')
+
+def test():
+    res = ingredients_used_per_week_total(1,2022,16)
+
+    for i in res:
+        print(i)
+
+if __name__ == '__main__':
+    test()
