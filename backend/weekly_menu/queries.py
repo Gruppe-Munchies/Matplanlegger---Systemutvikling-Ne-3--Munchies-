c9c44c3fe2e41853eda1a9098cbedf1bf7993fcd
Silje Aas <saa049@uit.no>
2022-04-19 18:09:36 +0200

    .

--- backend/weekly_menu/queries.py
+++ backend/weekly_menu/queries.py
@@ -12,13 +12,6 @@ def insert_to_weeklymenu(name, description, usergroup):
     session.add(new_weeklyMenu)
     session.commit()
 
-def insert_to_recipe_has_weeklymenu(menu_id, recipe_id, quantity):
-    session = loadSession()
-    new_weekly_menu_recipe = RecipeHasWeeklyMenu(weeklyMenu_idWeeklyMenu=menu_id, recipe_idRecipe=recipe_id,
-                                                 expectedConsumption=quantity)
-    session.add(new_weekly_menu_recipe)
-    session.commit()
-
 
 def insert_to_weekly_menu_date(menu_id, year, week):
     session = loadSession()
@@ -35,17 +28,13 @@ def fetch_recipes_where_usergroupid(usergroupId):
     session = loadSession()
     return session.query(Recipe).where(Recipe.userGroup_iduserGroup == usergroupId).all()
 
+
 def insert_to_recipe_has_weeklymenu(menu_id, recipe_id, quantity):
     session = loadSession()
     new_weekly_menu_recipe = RecipeHasWeeklyMenu(weeklyMenu_idWeeklyMenu=menu_id, recipe_idRecipe=recipe_id,
                                                  expectedConsumption=quantity)
     session.add(new_weekly_menu_recipe)
     session.commit()
-
-def fetch_weeklymenu_where_name_and_usergroupid(usergroup_id, menu_name):
-    session = loadSession()
-    return session.query(WeeklyMenu).filter(
-        and_(WeeklyMenu.userGroup_iduserGroup == usergroup_id, WeeklyMenu.name == menu_name)).all()
 
 
 def fetch_menu_name_where_menu_id(menu_id):
@@ -75,6 +64,18 @@ def fetch_ingrdients_where_recipe_id(recipe_id):
         RecipeHasIngredient.recipe_idRecipe == recipe_id).all()
 
 
+def fetch_ingrdients_with_costs_where_recipe_id(recipe_id):
+    session = loadSession()
+    res = session.query(RecipeHasIngredient.quantity, Ingredient.ingredientName, Ingredient.idingredient,
+                        UsergroupHasIngredient.price, UsergroupHasIngredient.unit,
+                        UsergroupHasIngredient.quantity).join(RecipeHasIngredient,
+                                                              RecipeHasIngredient.ingredient_idingredient == Ingredient.idingredient).join(
+        UsergroupHasIngredient, UsergroupHasIngredient.ingredient_idingredient == Ingredient.idingredient).filter(
+        RecipeHasIngredient.recipe_idRecipe == recipe_id).all()
+    session.close()
+    return res
+
+
 # ikke en query, funksjon som bruker queries, kan sikkert flyttes til der den skal brukes
 def get_all_ingredients_and_quantities_in_weeklymenu(menu_id):
     session = loadSession()
@@ -99,14 +100,52 @@ def get_all_ingredients_and_quantities_in_weeklymenu(menu_id):
     # index: 0-ingredientID, 1-ingredientName, 2-Quantity of ingredient in weekly menu
     return ingredientsList
 
+
+# ikke en query, funksjon som bruker queries, kan sikkert flyttes til der den skal brukes
+def get_all_ingredients_and_quantities_cost_etc_shopping_in_weeklymenu(menu_id):
+    session = loadSession()
+    recipes = fetch_recipes_where_weeklymenu_id(menu_id)
+    ingredientsList = []
+    for recipe in recipes:
+        resQuantity = recipe[1]
+        ingredients = fetch_ingrdients_with_costs_where_recipe_id(recipe[0])
+
+        for ingredient in ingredients:
+            exist = False
+            index = -1
+            for i in range(len(ingredientsList)):
+                if ingredientsList[i][1] == ingredient[1]:
+                    exist = True
+                    index = i
+
+            if exist:
+                quantity = (ingredient[0] * resQuantity) - ingredient[5]
+                cost = quantity * ingredient[3]
+                ingredientsList[index][2] += ingredient[0] * resQuantity
+                ingredientsList[index][4] = cost
+            else:
+                quantity = (ingredient[0] * resQuantity) - ingredient[5]
+                print("QUANTITY")
+                print(ingredient[0])
+                print(resQuantity)
+                print(ingredient[5])
+                print(quantity)
+                unit = ingredient[4]
+                id = ingredient[2]
+                name = ingredient[1]
+                cost = quantity * ingredient[3]
+                print("COST")
+                print(ingredient[3])
+                print(cost)
+                ingredientsList.append([id, name, quantity, unit, cost])
+
+    # index: 0-ingredientID, 1-ingredientName, 2-Quantity of ingredient in weekly menu, 3-unit, 4-totIngredientCost
+    return ingredientsList
+
+
 def fetch_menu_id_where_name(menu_name):
     session = loadSession()
     return session.query(WeeklyMenu.idWeeklyMenu).where(WeeklyMenu.name == menu_name).first()
-
-
-def fetch_recipes_where_usergroupid(usergroupId):
-    session = loadSession()
-    return session.query(Recipe).where(Recipe.userGroup_iduserGroup == usergroupId).all()
 
 
 def fetch_weeklymenu_where_name_and_usergroupid(usergroup_id, menu_name):
@@ -114,23 +153,35 @@ def fetch_weeklymenu_where_name_and_usergroupid(usergroup_id, menu_name):
     return session.query(WeeklyMenu).filter(
         and_(WeeklyMenu.userGroup_iduserGroup == usergroup_id, WeeklyMenu.name == menu_name)).all()
 
+
 def fetch_weeklymenu_where_usergroupid(usergroup_id):
     session = loadSession()
     return session.query(WeeklyMenu).filter((WeeklyMenu.userGroup_iduserGroup == usergroup_id)).all()
 
+
 def fetch_weeklymenu_recipes_where_name_usergroupid():
     session = loadSession()
-    return session.query(Recipe.name ,RecipeHasWeeklyMenu.expectedConsumption, RecipeHasWeeklyMenu.recipe_idRecipe).join(RecipeHasWeeklyMenu, Recipe.idRecipe == RecipeHasWeeklyMenu.recipe_idRecipe).all()
-
+    return session.query(Recipe.name, RecipeHasWeeklyMenu.expectedConsumption,
+                         RecipeHasWeeklyMenu.recipe_idRecipe).join(RecipeHasWeeklyMenu,
+                                                                   Recipe.idRecipe == RecipeHasWeeklyMenu.recipe_idRecipe).all()
 
 
 def remove_from_RecipeHasWeeklyMenu(recipeID):
     session = loadSession()
-    session.query(RecipeHasWeeklyMenu).filter(RecipeHasWeeklyMenu.recipe_idRecipe == recipeID).delete(synchronize_session=False)
+    session.query(RecipeHasWeeklyMenu).filter(RecipeHasWeeklyMenu.recipe_idRecipe == recipeID).delete(
+        synchronize_session=False)
     session.commit()
 
+
 if __name__ == '__main__':
-    insert_to_weekly_menu_date(2, 2022, 15)
+    rec = get_all_ingredients_and_quantities_cost_etc_shopping_in_weeklymenu(3)
+    for r in rec:
+        print(r[0])
+        print(r[1])
+        print(r[2])
+        print(r[3])
+        print(r[4])
+    # insert_to_weekly_menu_date(2, 2022, 15)
     # rec = fetch_menu_name_where_menu_id(27)
     # print(rec)
 #     rec = get_all_ingredients_and_quantities_in_weeklymenu(27)
