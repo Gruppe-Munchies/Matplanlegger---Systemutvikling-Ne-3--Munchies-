250ae719bf74b903ec78077c7d6d12a3478b5174
Silje Aas <saa049@uit.no>
2022-04-21 14:55:05 +0200

    rounding

--- backend/weekly_menu/queries.py
+++ backend/weekly_menu/queries.py
@@ -155,28 +155,16 @@ def get_all_ingredients_and_quantities_cost_etc_shopping_in_weeklymenu(menu_id):
             if exist:
                 quantity = (ingredient[0] * resQuantity) - ingredient[5]
                 cost = quantity * ingredient[3]
-                ingredientsList[index][2] += ingredient[0] * resQuantity
-                print(ingredientsList[index][4])
-                print(cost)
-                print(ingredientsList[index][4] + cost)
-                ingredientsList[index][4] += cost
-                print(ingredientsList[index][4])
+                ingredientsList[index][2] += round(ingredient[0] * resQuantity)
+                ingredientsList[index][4] += round(cost)
             else:
                 quantity = (ingredient[0] * resQuantity) - ingredient[5]
-                # print("QUANTITY")
-                # print(ingredient[0])
-                # print(resQuantity)
-                # print(ingredient[5])
-                # print(quantity)
                 price = ingredient[3]
                 unit = ingredient[4]
                 id = ingredient[2]
                 name = ingredient[1]
                 cost = quantity * ingredient[3]
-                # print("COST")
-                # print(ingredient[3])
-                # print(cost)
-                ingredientsList.append([id, name, round(quantity), unit, cost, price])
+                ingredientsList.append([id, name, round(quantity), unit, round(cost), price])
 
     # index: 0-ingredientID, 1-ingredientName, 2-Quantity of ingredient in weekly menu, 3-unit, 4-totIngredientCost, 5-unit price
     return ingredientsList
