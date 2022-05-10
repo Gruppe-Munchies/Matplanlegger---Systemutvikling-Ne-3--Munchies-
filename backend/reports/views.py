9bbafbd043448627f528955dc1a1a5a1e1eb89e6
Jan Erik Bisseth <jbi017@uit.no>
2022-05-10 21:18:55 +0200

    Rewrite route to use session variable for menuId

--- backend/reports/views.py
+++ backend/reports/views.py
@@ -6,24 +6,26 @@ from flask_login import login_required
 
 reports = Blueprint('reports', __name__, template_folder='templates')
 
+
 def test():
     pd.set_option('display.max_columns', None)
     pd.set_option('display.expand_frame_repr', False)
 
-    res2 = pd.read_sql(ingredients_used_per_week_total(3,2022,19).statement, engine)
-    print(res2[['Ukemeny','Gruppe', 'Ingrediens', 'SumMengde', 'Enhet', 'SumBelop']])
+    res2 = pd.read_sql(ingredients_used_per_week_total(3, 2022, 19).statement, engine)
+    print(res2[['Ukemeny', 'Gruppe', 'Ingrediens', 'SumMengde', 'Enhet', 'SumBelop']])
 
     print("")
     print("")
 
-    res = pd.read_sql(ingredients_used_per_week_per_dish(3,2022,19,4).statement, engine)
-    print(res[['Ukemeny','Gruppe', 'Oppskrift', 'Ingrediens', 'Mengde', 'Prognose', 'SumMengde', 'Enhet']])
+    res = pd.read_sql(ingredients_used_per_week_per_dish(3, 2022, 19, 4).statement, engine)
+    print(res[['Ukemeny', 'Gruppe', 'Oppskrift', 'Ingrediens', 'Mengde', 'Prognose', 'SumMengde', 'Enhet']])
 
     print("")
     print("")
 
     res3 = pd.read_sql(fetch_weekly_menus_for_group(3).statement, engine)
     print(res3)
+
 
 if __name__ == '__main__':
     test()
@@ -32,7 +34,6 @@ if __name__ == '__main__':
 @reports.route('/rapporter', methods=['GET', 'POST'])
 @login_required
 def ingredients_used_per_week():
-
     groupId = session.get('group_to_use')
     menuId = session.get('menuId')
 
@@ -40,6 +41,7 @@ def ingredients_used_per_week():
         session['menuId'] = request.args["menu"]
         menuId = session.get('menuId')
 
+    totalcost = 0
     recipes = []
     perRecipe = False
 
@@ -50,19 +52,20 @@ def ingredients_used_per_week():
         recipeId = request.args["recipe"]
         res = ingredients_used_per_week_per_dish(menuId, recipeId)
         perRecipe = True
+        for i in res:
+            totalcost += res[0].SumBelop
 
     # Else if menu selected
     elif 'menu' in request.args:
         res = ingredients_used_per_week_total(menuId)
+        for i in res:
+            totalcost += res[0].SumBelop
 
     else:
         res = ingredients_used_per_week_total(0)
-        totalcost = 0
 
     if 'weeknum' in request.args:
         recipes = fetch_recipes_in_weekly_menu(menuId)
-        totalcost = 0
-        for i in res:
-           totalcost += res[0].SumBelop
 
-    return render_template('report.html', report=res, menus=menus, recipes=recipes, totalcost=totalcost, perRecipe=perRecipe)
+    return render_template('report.html', report=res, menus=menus, recipes=recipes, totalcost=totalcost,
+                           perRecipe=perRecipe)
